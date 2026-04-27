08b0e22ad44553f7d7711251d4a4ee0ec26edbb1
Tipochec <www.marat12345k@mail.ru>
2026-04-28 00:21:48 +1000

    Создание бд и использование сайта pandascore в качестве api

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import os
 import requests
+import json
 from dotenv import load_dotenv
 load_dotenv()
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36"
-}
+API_KEY: str = os.getenv("API_KEY")
+BASE_URL = f"https://api.pandascore.co/csgo/matches?token={API_KEY}"
 
-response = requests.get()
+response = requests.get(BASE_URL)
+data = response.json()
+
+
+for match in data:
+    team1 = match["opponents"][0]["opponent"]["name"]
+    team2 = match["opponents"][1]["opponent"]["name"]
+    status = match["status"]
+    score1 = match["results"][0]["score"]
+    score2 = match["results"][1]["score"]
+    tournament = match["league"]["name"]
+    date = match["scheduled_at"]
+
+    print(f"{team1} vs {team2} | {status} | {score1}:{score2} | {tournament} | {date}")
+
